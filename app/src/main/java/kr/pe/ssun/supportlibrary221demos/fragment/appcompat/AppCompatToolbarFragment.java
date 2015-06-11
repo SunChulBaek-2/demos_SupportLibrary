0f877c5935c9057442f39a25cf6e6f701d1c54b3
x1210x <x1210x@gmail.com>
2015-06-11 18:53:27 +0900

    update Toolbar

--- app/src/main/java/kr/pe/ssun/supportlibrary221demos/fragment/appcompat/AppCompatToolbarFragment.java
+++ app/src/main/java/kr/pe/ssun/supportlibrary221demos/fragment/appcompat/AppCompatToolbarFragment.java
@@ -2,6 +2,7 @@ package kr.pe.ssun.supportlibrary221demos.fragment.appcompat;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,15 @@ public class AppCompatToolbarFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 final Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_appcompat_toolbar, container, false);
+
+		Toolbar toolbar = (Toolbar)rootView.findViewById(R.id.toolbar);
+		toolbar.setNavigationIcon(R.drawable.ic_action_navigation_menu);
+		toolbar.setLogo(R.drawable.ic_launcher);
+		toolbar.setTitleTextColor(R.color.darkTextPrimary);
+		toolbar.setTitle("Title");
+		toolbar.setSubtitleTextColor(R.color.darkTextSecondary);
+		toolbar.setSubtitle("SubTitle");
+		toolbar.inflateMenu(R.menu.menu_appbar_layout);
 
 		return rootView;
 	}
