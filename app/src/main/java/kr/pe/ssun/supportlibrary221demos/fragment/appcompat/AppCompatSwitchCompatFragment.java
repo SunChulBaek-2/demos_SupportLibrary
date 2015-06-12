05831716ec6cffd77551a438560b18c5f07ba50e
x1210x <x1210x@gmail.com>
2015-06-12 17:50:04 +0900

    add SwitchCompat

--- app/src/main/java/kr/pe/ssun/supportlibrary221demos/fragment/appcompat/AppCompatSwitchCompatFragment.java
+++ app/src/main/java/kr/pe/ssun/supportlibrary221demos/fragment/appcompat/AppCompatSwitchCompatFragment.java
@@ -2,6 +2,7 @@ package kr.pe.ssun.supportlibrary221demos.fragment.appcompat;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,9 @@ public class AppCompatSwitchCompatFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 final Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_appcompat_switch_compat, container, false);
+
+		SwitchCompat onOff = (SwitchCompat)rootView.findViewById(R.id.onOff);
+		onOff.setShowText(true);
 
 		return rootView;
 	}
