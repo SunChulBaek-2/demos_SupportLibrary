2f0cb3031eea2afb66b3171cd6090fba17980511
x1210x <x1210x@gmail.com>
2015-06-18 22:40:37 +0900

    DrawerLayout, SlidingPaneLayout

--- app/src/main/java/kr/pe/ssun/supportlibrary221demos/fragment/support/SupportDrawerLayoutFragment.java
+++ app/src/main/java/kr/pe/ssun/supportlibrary221demos/fragment/support/SupportDrawerLayoutFragment.java
@@ -2,9 +2,11 @@ package kr.pe.ssun.supportlibrary221demos.fragment.support;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import kr.pe.ssun.supportlibrary221demos.R;
 
@@ -16,6 +18,17 @@ public class SupportDrawerLayoutFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 final Bundle savedInstanceState) {
 		final View rootView = inflater.inflate(R.layout.fragment_support_drawer_layout, container, false);
+
+		final DrawerLayout drawer = (DrawerLayout)rootView.findViewById(R.id.drawer);
+		final View rlDrawer = rootView.findViewById(R.id.rlDrawer);
+		Button btnOpenDrawer = (Button) rootView.findViewById(R.id.btnOpenDrawer);
+
+		btnOpenDrawer.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				drawer.openDrawer(rlDrawer);
+			}
+		});
 
 		return rootView;
 	}
--- app/src/main/java/kr/pe/ssun/supportlibrary221demos/fragment/support/SupportSlidingPaneLayoutFragment.java
+++ app/src/main/java/kr/pe/ssun/supportlibrary221demos/fragment/support/SupportSlidingPaneLayoutFragment.java
@@ -2,9 +2,11 @@ package kr.pe.ssun.supportlibrary221demos.fragment.support;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import kr.pe.ssun.supportlibrary221demos.R;
 
@@ -16,6 +18,45 @@ public class SupportSlidingPaneLayoutFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 final Bundle savedInstanceState) {
 		final View rootView = inflater.inflate(R.layout.fragment_support_sliding_pane_layout, container, false);
+
+		final SlidingPaneLayout slidingPaneLayout = (SlidingPaneLayout)rootView.findViewById(R.id.slidingpane);
+		final Button btnOpenLeftPane = (Button)rootView.findViewById(R.id.btnOpenLeftPane);
+
+		slidingPaneLayout.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
+			@Override
+			public void onPanelSlide(View panel, float slideOffset) {
+
+			}
+
+			@Override
+			public void onPanelOpened(View panel) {
+				btnOpenLeftPane.setText("CLOSE LEFT PANE");
+				btnOpenLeftPane.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						slidingPaneLayout.closePane();
+					}
+				});
+			}
+
+			@Override
+			public void onPanelClosed(View panel) {
+				btnOpenLeftPane.setText("OPEN LEFT PANE");
+				btnOpenLeftPane.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						slidingPaneLayout.openPane();
+					}
+				});
+			}
+		});
+
+		btnOpenLeftPane.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				slidingPaneLayout.openPane();
+			}
+		});
 
 		return rootView;
 	}
